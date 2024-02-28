27e6a04cb57262c6ef4b5bc55b3f53c131860969
Ronnie Valenzuela <onie.valenzuelaaa02@gmail.com>
2024-02-29 01:09:02 +0800

    final work

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const swiper = new Swiper(".swiper-container", {
-        // Optional Swiper configuration
-        loop: true, // Enable looping
-        slidesPerView: "auto", // Auto-adjust slides per view
-        spaceBetween: 20, // Adjust the space between slides as needed
-        // Add more configurations as needed
-    });
-});
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+
+// Cursor
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function(e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`,
+
+    }, {duration: 500, fill: "forwards"})
+})
